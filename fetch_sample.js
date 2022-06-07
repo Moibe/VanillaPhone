c4389b6be42cf544a72040e261a403d37833ef36
Moibe <moi_estrello@hotmail.com>
2022-06-07 00:51:55 -0500

    Eliminación de copias en conflicto

--- fetch_sample.js
+++ fetch_sample.js
@@ -1,59 +1,90 @@
+document.getElementById("submit_mint").onclick = hacer;
+
 function hacer(){
 
-  //obtenTzid es una promesa, aquí la estamos declarando.
-  var obtenTzid = new Promise(
-  
-    function(resolve, reject){
-      
-      fetch('https://onlinesim.ru/api/getNum.php?apikey=ba4948c08e3d1494b4d11c437a29c663&service=WhatsApp&country=7')
-      .then(response => response.json())
-      .then(data => 
-        {if(data.response == 1)
-        { console.log("Si obtuvo un servicio...");
-          resolve("OK!")}
-        else 
-        {
-          console.log("No se obtuvo el servicio...");
-          reject(Error("It broke"))}
-        })
-        
-    });
+  var tzid = "";
+  var numero = "";
 
-  //obtenNumero es una promesa, aquí la estamos declarando.
-  var obtenNumero = new Promise(
+//obtenTzid es una promesa, aquí la estamos declarando...
+var obtenTzid = new Promise(
   
-    function(resolve, reject){
+  function(resolve, reject){
+    
+    fetch('https://onlinesim.ru/api/getNum.php?apikey=ba4948c08e3d1494b4d11c437a29c663&service=WhatsApp&country=33')
+    .then(response => response.json()) //convierte la respuesta en datos.
+    .then(data => 
+      {if(data.response == 1) //Si data.response tiene un 1 significa que si se otorgó el servicio.
+      { console.log("Si obtuvo un servicio:");
+        console.log(data);
+        tzid = data.tzid; //Guarda el tzid en la var resultado.
+        resolve(tzid)}
+      else 
+      {
+        console.log("No se obtuvo el servicio...");
+        tzid = data.response; 
+        reject(Error(tzid))}
+      })
       
-      fetch('http://onlinesim.ru/api/getState.php?apikey=ba4948c08e3d1494b4d11c437a29c663&tzid='+data.tzid)
-      .then(response => response.json())
-      .then(data => 
-        {if(data.response == 1)
-        { console.log("Éste segundo Si se resolvió...");
-          resolve("OK!")}
-        else 
-        {
-          console.log("Éste segundo No se resolvió...");
-          reject(Error("It broke"))}
-        })
-        
-    });
-
-  //Ejecución General...
-  obtenTzid.then(
-    function(result) {
-    //Si ésta promesa se cumple, entonces...
-    console.log(result); // "Stuff worked!"
-    //"Aquí ejecutará la promesa de obtenNumero..."
-    }, 
-    //Si ésta promesa no se logra cumplir, entonces...
-    function(err) {
-    console.log(err); // Error: "It broke"
-    console.log("It broke...")
   });
 
+ 
+  //Ejecución basado en el resultado de la promesa...
+  obtenTzid.then
+  (
+    function(result) //Si ésta promesa se cumple, entonces...
+    {
+    console.log(tzid); //Despliega el tzid obtenido.
+    //Aquí irá anidada la promesa de checar número en el segundo chequeo, 
+    //solo si se logra la primer promesa de obtener el servicio.
+
+    //obtenNumero es una promesa, aquí la estamos declarando...
+var obtenNumero = new Promise(
+
+  function(resolve, reject){
+    
+    fetch('http://onlinesim.ru/api/getState.php?apikey=ba4948c08e3d1494b4d11c437a29c663&tzid='+tzid)
+    .then(response => response.json()) //convierte la respuesta en datos.
+    .then(data => 
+      {if(data[0].response == 'TZ_NUM_WAIT') //Si data.response tiene un TZ_NUM_WAIT significa que si se otorgó el servicio.
+      { console.log("Si obtuvo el número:");
+        console.log(data);
+        numero = data[0].number; //Guarda el tzid en la var resultado.
+        resolve(numero)}
+      else 
+      {
+        console.log("No se obtuvo el número...");
+        numero = data.response; 
+        reject(Error(numero))}
+      })
+      
+  });
+
+    //Ejecución basada en el resultado de la segunda promesa. 
+    obtenNumero.then
+    (
+    function(result) //Si ésta promesa se cumple, entonces...
+    {
+    console.log("El número obtenido es:");
+    console.log(numero); //Despliega el numero obtenido.
+    }, 
+    
+    function(err) //Si la segunda promesa no se logra cumplir, entonces...
+    {
+    console.log(err); //Error...
+    console.log(numero)
+    }
+    );
+
+    }, 
+    
+    function(err) //Si la primer promesa no se logra cumplir, entonces...
+    {
+    console.log(err); //Error...
+    console.log(tzid)
+    }
+  );
 
   
     
 }
 
-document.getElementById("submit_mint").onclick = hacer;
